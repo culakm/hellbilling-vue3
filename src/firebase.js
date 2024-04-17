1747da1214ed68eaedfa33e66c89367e23d38efd
Martin Culak <culak.martin@gmail.com>
2024-04-17 22:13:42 +0200

    api_key je v .env.local

--- src/firebase.js
+++ src/firebase.js
@@ -1,29 +1,30 @@
 import { initializeApp } from 'firebase/app';
-import { getDatabase } from 'firebase/database';
+// import { getDatabase } from 'firebase/database';
+// import { getFirestore } from 'firebase/firestore';
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
-import { getFunctions } from 'firebase/functions';
+// import { getFunctions } from 'firebase/functions';
 
 const firebaseConfig = {
-	apiKey: "AIzaSyB8AXo6D4Vq4MKetOc5s4psrHwjF87F15A",
-	authDomain: "hellbilling1.firebaseapp.com",
-	databaseURL: "https://hellbilling1-default-rtdb.europe-west1.firebasedatabase.app",
-	projectId: "hellbilling1",
-	storageBucket: "hellbilling1.appspot.com",
-	messagingSenderId: "76635219533",
-	appId: "1:76635219533:web:dc0d0f6efa9278424d1e04"
+	apiKey: import.meta.env.VITE_API_KEY,
+	authDomain: "hellbilling3.firebaseapp.com",
+	projectId: "hellbilling3",
+	storageBucket: "hellbilling3.appspot.com",
+	messagingSenderId: "929544538371",
+	appId: "1:929544538371:web:6f17ef132fb3a0cf3d4136"
+	// databaseURL: "https://hellbilling1-default-rtdb.europe-west1.firebasedatabase.app", toto nie je vygenerovane
 };
 
 const app = initializeApp(firebaseConfig);
-export const db = getDatabase();
+// export const db = getDatabase();
 export const auth = getAuth(app);
-export const functionsFB = getFunctions(app);
+// export const functionsFB = getFunctions(app);
 
 // kontrola stavu prihlasenia
 onAuthStateChanged(auth, (user) => {
 	if (user) {
 		// const uid = user.uid;
-		//alert('User is signed in');
+		alert('User is signed in');
 	} else {
-		//alert('User is signed out');
+		alert('User is signed out');
 	}
 });
